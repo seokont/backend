e5c49e34c80ff8d64a857d75bd206a07cd387319
Maksym <seokont@gmail.com>
2023-07-02 21:32:48 +0300

    fixed db connect

--- connect/db.js
+++ connect/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 // Подключение к MongoDB
 export const db = {
-  connect: "mongodb://localhost:27017/likar_database",
+  connect: "mongodb+srv://dizainzt:Maqim1981@cluster0.yamoyz7.mongodb.net/",
   option: {
     useNewUrlParser: true,
     useUnifiedTopology: true,
