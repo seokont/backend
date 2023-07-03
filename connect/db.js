41e64a07a9c01b9c34978981e65d10b49c06de57
Maksym <seokont@gmail.com>
2023-07-03 12:37:17 +0300

    fixed db

--- connect/db.js
+++ connect/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 // Подключение к MongoDB
 export const db = {
-  connect: "mongodb+srv://dizainzt:Maqim1981@cluster0.yamoyz7.mongodb.net/",
+  connect: "mongodb+srv://user2000:maqim1981@cluster0.yamoyz7.mongodb.net/",
   option: {
     useNewUrlParser: true,
     useUnifiedTopology: true,
