747ee164cbce3a042ec979196ad8e6d23db37777
Maksym <seokont@gmail.com>
2023-07-03 15:45:33 +0300

    fixed k

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ app.put("/", saveNewAppo);
 app.delete("/", functionDelete);
 
 // Запуск сервера
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
+app.listen(80, () => {
+  console.log("Server is running on port 80");
 });
